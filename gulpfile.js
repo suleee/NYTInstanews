71a49e09e056b1328cf073b6cc02ec8b34b6d702
cyu1188 <cyu1188@gmail.com>
2017-01-26 11:47:38 -0800

    created mobile page

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp'); // Load Gulp!
     rename = require('gulp-rename'),
     browserSync = require('browser-sync').create();
     eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify');
 
@@ -17,7 +20,7 @@ var plumberErrorHandler = {
 
 gulp.task('scripts', function(){
   gulp.src('./js/*.js') // What files do we want gulp to consume?
-//.pipe(plumber(plumberErrorHandler))
+   .pipe(plumber(plumberErrorHandler))
    .pipe(uglify()) // Call the uglify function on these files
    .pipe(rename({ extname: '.min.js' })) // Rename the uglified file
    .pipe(gulp.dest('./build/js')) // Where do we put the result?
@@ -30,15 +33,27 @@ gulp.task('browser-sync', function() {
         }
     });
 
-    gulp.watch(['style.css'], ['build/js/*js']).on('change', browserSync.reload);
+    gulp.watch(['build/css/*.css', 'build/js/*js']).on('change', browserSync.reload);
 });
 
 gulp.task('lint', function() {
     return gulp.src(['./js/*.js'])
-        //.pipe(plumber(plumberErrorHandler))
+        .pipe(plumber(plumberErrorHandler))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('default', ['lint'], function () {
@@ -46,6 +61,7 @@ gulp.task('default', ['lint'], function () {
 
 gulp.task('watch', function() {
    gulp.watch('js/*.js', ['scripts']);
+   gulp.watch('sass/*.scss', ['sass']);
 });
 
 gulp.task('default',['watch', 'browser-sync']);
