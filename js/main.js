285d3c581a714e36f2b073c4c7549839876ef542
cyu1188 <cyu1188@gmail.com>
2017-01-27 15:04:11 -0800

    added API key to main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-$(function(){
-
-  //this is my message
-  var message = 'Hello World'
-  console.log("need food");
-
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "ea765f54b5984dd9a7ec3fd93101fdcc"
 });
+
+$(function() {
+  $('form').on('option', function() {
+     event.preventDefault(); // stops page refresh
+     $('.album-list').empty(); // clears list if searching again
+      $.ajax({
+     method: 'GET',
+     url: 'https://api.nytimes.com/svc/topstories/v2/home.json'+input,
+     dataType: 'jsonp'
+       })
+     .done(function(data) {
+        console.log(data)
+        if (data.resultCount !== 0) {
+          $.each(data.results, function (index, value) {
+          $('.album-list').append('<div class="results_wrap"><h1 class="h1result">'+value.collectionName+'</h1><img src='+value.artworkUrl100+'/></div>');
+           })
+        }
+        else {
+          $('.album-list').append('<h1 class="h1result">Couldnt find anything, Yo</h1>');
+        }
+      })
+     .fail(function() {
+        alert('FAIL!');
+      })
+     .always(function(){
+      $('.h1loading').hide();
+     })
+ });
+
+ });
