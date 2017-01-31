7b46b78da95cec61ff650fdadb1a8c1ddba934ec
cyu1188 <cyu1188@gmail.com>
2017-01-30 17:31:33 -0800

    articles now displays working on call stack size

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+ var list= '';
   $('select').on('change', function(event) {
     event.preventDefault()
     var userSelect = $('.topics').val()
@@ -9,13 +9,31 @@ $(document).ready(function() {
       url:'http://api.nytimes.com/svc/topstories/v2/' + userSelect + '.json?api-key=ea765f54b5984dd9a7ec3fd93101fdcc',
     }).done(function(data){
       console.log(data);
-      if (data.results.length, function) (index, value) {
-          $('.articles-list').empty()
+      $('.article-list').empty()
+      if (data.results.length) {
+
+
+        $.each(data.results, function (index, results) {
+          console.log('Got result')
+
+          if(results.multimedia.length){
+              list += '<div class="article-list"><li><a href="' + results.url +'"  <p>' + results.abstract + '</p><img src="' + results.multimedia[4].url + '"/></li></div>'
+          }
+        })
+
+        $('.topics')
+        // console.log(index);
+        // console.log(results);
+
+        // $('.article-list').append('<div class="article-list"><li><a href="' + results.url +'"  <p>' + results.abstract + '</p><img src="' + results.multimedia[4].url + '"/></li></div>');
+        //  })
+
           //get the abstract,
           //get the imageurl
           //only get news with images using if statement
       }
       else {
+        $('.article-list').append('Sorry no articles on this subject today!')
         // handle no results
       }
     })
